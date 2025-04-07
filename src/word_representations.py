b7ed2cc77fd5cacbb9ef633cefad75d85b361feb
Louis Jalouzot <86967472+LouisJalouzot@users.noreply.github.com>
2025-04-07 18:20:11 +0200

    Add WordRepresentations class and integrate into main notebook; refactor word processing functions for improved data handling

--- src/word_representations.py
+++ src/word_representations.py
@@ -14,6 +14,7 @@ def make_sentence(words):
     s = ""
     cum_s = []
     indices = []
+    df = None
     for word in words:
         if word in string.punctuation:
             s = s.strip()
@@ -25,33 +26,33 @@ def make_sentence(words):
     return (s.strip(), indices, cum_s)
 
 
-def events_from_words(words: pd.DataFrame) -> pd.DataFrame:
-    events = words.copy()
-    events["sentence"] = events.groupby("sentence_id").word.transform(
+def df_from_words(words: pd.DataFrame) -> pd.DataFrame:
+    df = words.copy()
+    df["sentence"] = df.groupby("sentence_id").word.transform(
         lambda words: make_sentence(words)[0]
     )
-    events["sentence_char"] = events.groupby("sentence_id").word.transform(
+    df["sentence_char"] = df.groupby("sentence_id").word.transform(
         lambda words: make_sentence(words)[1]
     )
-    events["context"] = events.groupby("sentence_id").word.transform(
+    df["context"] = df.groupby("sentence_id").word.transform(
         lambda words: make_sentence(words)[2]
     )
-    events = events.rename(columns={"word": "text"})
-    events["timeline"] = "svo_word_level"
-    events["language"] = "en"
-    events["type"] = "Word"
-    events["start"] = events.index
-    events["duration"] = 0.5
-    events = ns.segments.validate_events(events)
+    df = df.rename(columns={"word": "text"})
+    df["timeline"] = "svo_word_level"
+    df["language"] = "en"
+    df["type"] = "Word"
+    df["start"] = df.index
+    df["duration"] = 0.5
+    df = ns.segments.validate_events(df)
 
-    return events
+    return df
 
 
 class WordRepresentations(BaseModel):
     model_name: str = "gpt2"
     token_aggregation: str = "mean"
     batch_size: int = 32
-    layer: int = 5
+    layer: float = 5 / 13
     units: tp.List[int] = None
     norm: tp.Optional[int] = None
 
@@ -60,20 +61,21 @@ class WordRepresentations(BaseModel):
     model_config: ConfigDict = ConfigDict(extra="forbid")
 
     def compute_representations(self, words: pd.DataFrame) -> torch.Tensor:
-        events = events_from_words(words)
         feature = HuggingFaceText(
             contextualized=True,
             token_aggregation="mean",
             model_name=self.model_name,
             device=self._device,
             batch_size=self.batch_size,
-            layer=self.layer,
+            layers=self.layer,
             infra=self.infra,
             cache_all_layers=True,
         )
-        data = feature._get_data([row for _, row in events.iterrows()])
-        data = list(data)
-        data = np.array(data)
-        data = torch.Tensor(data)
+        df = df_from_words(words)
+        events = feature._events_from_dataframe(df)
+        data = feature._get_timed_arrays(
+            events, start=0, duration=df.stop.max()
+        )
+        data = np.array([ta.data for ta in data])
 
-        return data
+        return torch.Tensor(data)
